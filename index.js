c33c8bc0ed71d18f42f5909fa75d14aa7a84345a
mark langan <markelangan@gmail.com>
2023-04-26 20:11:13 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+//function saturdayFun(activity){
+ //if (typeof activity === 'string'){
+    //return `This Saturday, I want to ${activity}!`
+ //} else {
+    //return 'This Saturday, I want to roller-skate!'
+//}} 
+//function mondayWork(activity){
+    //if (typeof activity === 'string'){
+       //return `This Monday, I will ${activity}.`
+    //} else {
+       //return 'This Monday, I will go to the office.'
+   //}} 
+
+function wrapAdjective(flair="*"){
+    return function (adjective='special') {
+        return `You are ${flair}${adjective}${flair}!`
+    } 
+ }
+
+function saturdayFun(activity='roller-skate'){
+       return `This Saturday, I want to ${activity}!`}
+
+function mondayWork (activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
